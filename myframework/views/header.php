30ea6ce255303e9ade301377ece8b372aa64b163
Toby Horn <kankuro865@gmail.com>
2018-03-06 16:41:23 -0600

    Added Bootstrap and Bootstrap template

--- myframework/views/header.php
+++ myframework/views/header.php
@@ -1,1 +1,16 @@
-<header style="height:100px; width: 100%; background-color: #333; color:#999; text-align: center;"><h1 style="margin:0px; padding-top: 25px;">Header</h1></header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Toby Horn SSL</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
